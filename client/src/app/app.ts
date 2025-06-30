7e8a3cee08975f30665dd810a340a63590ac671d
Kire Smilkov <kiresmilkov14@gmail.com>
2025-06-30 18:17:30 +0200

    Angular

---
+++ client/src/app/app.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+
+  http = inject(HttpClient);
+  protected title = 'Dating App';
+  users: any;
+
+    ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Bravo')
+    })
+  }
+}
